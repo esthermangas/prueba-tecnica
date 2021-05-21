37f6b1e7c60457866ed54f026700271ed798cdec
Esther <esthermangas95@gmail.com>
2021-05-21 12:08:16 +0200

    created a Automate a process that performs a search on the website of the Generalitat de Catalunya

---
+++ index.js
@@ -0,0 +1,11 @@
+const { chromium } = require('playwright');
+
+(async () => {
+    const browser = await chromium.launch({ headless: false, slowMo: 50 });
+    const page = await browser.newPage('https://web.gencat.cat/ca/inici/');
+    await page.goto('https://web.gencat.cat/ca/inici/');
+    await page.fill('id=cercadorOcultGoogle', 'agenda cultural' );
+    await page.click('.btn');
+    // Create pages, interact with UI elements, assert values
+    await browser.close();
+})();
